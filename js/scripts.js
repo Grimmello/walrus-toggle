f31b2ae821b2571e90a7b1594ec8953bf721a983
Clifford Grimmell and Cameron Jensen <cameron@>
2017-02-14 10:03:42 -0800

    Add slide up and down for a picture

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,14 @@ $(function() {
     $("#walrus-showing").toggle();
     $("#walrus-hidden").toggle();
   });
+  $(".clickable2").click(function() {
+    $(".panel2").toggle();
+    $(".panel").toggle();
+  });
+  $(".clickable3").click(function(){
+    $(".randomimage").slideUp();
+  });
+  $(".clickable4").click(function(){
+    $(".randomimage").slideDown();
+  });
 });
